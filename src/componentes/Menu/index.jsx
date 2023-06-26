2d13e3ca324ee7568f5870ac61a165f10a338fdf
William Fischdick <jwfbit@hotmail.com>
2023-06-25 22:34:58 -0300

    :construction: Criando o componente do card e alterando as pastas dos ícones

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,37 +1,14 @@
-import React from 'react'
-import styles from './Menu.module.scss'
-import home from '../../assets/icones/home.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas.png'
-import maisVistas from '../../assets/icones/mais-vistas.png'
-import novas from '../../assets/icones/novas.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me.png'
+import React from 'react';
+import style from './Menu.module.scss';
+import icones from './icones.json';
+import Icone from './Icone';
 
-
-export default function Menu () {
-    return (
-        <nav className={styles.menu}>
-            <ul className={styles.menu__lista}>
-                <li className={styles.menu__item}>
-                    <img src={home} alt="icone home"></img>
-                    <a href="#">Início</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisCurtidas} alt="icone mais curtidas"></img>
-                    <a href="#">Mais Curtidas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisVistas} alt="icone mais vistas"></img>
-                    <a href="#">Mais Vistas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={novas} alt="icone novas"></img>
-                    <a href="#">Novas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={surpreendaMe} alt="icone surpreenda-me"></img>
-                    <a href="#">Surpreenda-me</a>
-                </li>
-            </ul>
-        </nav>
-    )
+export default function Menu() {
+  return (
+    <nav className={style.menu}>
+      <ul className={style.menu__lista}>
+        {icones.map(icone => (<Icone key={icone.id} icone={icone} style={style} />))}
+      </ul>
+    </nav>
+  )
 }
